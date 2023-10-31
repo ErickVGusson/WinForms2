4bb5cfa63a1abfe68df507e214afcfd9ed1538be
ErickVGusson <92764323+ErickVGusson@users.noreply.github.com>
2023-10-31 11:23:38 -0300

    Organizadores

--- WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -28,13 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnTroca = new System.Windows.Forms.Button();
+            this.txbValor1 = new System.Windows.Forms.TextBox();
+            this.txbValor2 = new System.Windows.Forms.TextBox();
+            this.txbValor3 = new System.Windows.Forms.TextBox();
+            this.btnDecrescente = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTroca
+            // 
+            this.btnTroca.Location = new System.Drawing.Point(12, 64);
+            this.btnTroca.Name = "btnTroca";
+            this.btnTroca.Size = new System.Drawing.Size(100, 23);
+            this.btnTroca.TabIndex = 0;
+            this.btnTroca.Text = "Trocar valores";
+            this.btnTroca.UseVisualStyleBackColor = true;
+            this.btnTroca.Click += new System.EventHandler(this.btnTroca_Click);
+            // 
+            // txbValor1
+            // 
+            this.txbValor1.Location = new System.Drawing.Point(12, 12);
+            this.txbValor1.Name = "txbValor1";
+            this.txbValor1.Size = new System.Drawing.Size(100, 20);
+            this.txbValor1.TabIndex = 1;
+            // 
+            // txbValor2
+            // 
+            this.txbValor2.Location = new System.Drawing.Point(118, 12);
+            this.txbValor2.Name = "txbValor2";
+            this.txbValor2.Size = new System.Drawing.Size(100, 20);
+            this.txbValor2.TabIndex = 2;
+            // 
+            // txbValor3
+            // 
+            this.txbValor3.Location = new System.Drawing.Point(224, 12);
+            this.txbValor3.Name = "txbValor3";
+            this.txbValor3.Size = new System.Drawing.Size(100, 20);
+            this.txbValor3.TabIndex = 3;
+            // 
+            // btnDecrescente
+            // 
+            this.btnDecrescente.Location = new System.Drawing.Point(118, 64);
+            this.btnDecrescente.Name = "btnDecrescente";
+            this.btnDecrescente.Size = new System.Drawing.Size(100, 23);
+            this.btnDecrescente.TabIndex = 4;
+            this.btnDecrescente.Text = "Decrescente";
+            this.btnDecrescente.UseVisualStyleBackColor = true;
+            this.btnDecrescente.Click += new System.EventHandler(this.btnDecrescente_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(479, 287);
+            this.Controls.Add(this.btnDecrescente);
+            this.Controls.Add(this.txbValor3);
+            this.Controls.Add(this.txbValor2);
+            this.Controls.Add(this.txbValor1);
+            this.Controls.Add(this.btnTroca);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnTroca;
+        private System.Windows.Forms.TextBox txbValor1;
+        private System.Windows.Forms.TextBox txbValor2;
+        private System.Windows.Forms.TextBox txbValor3;
+        private System.Windows.Forms.Button btnDecrescente;
     }
 }
 
--- WindowsFormsApp2/WindowsFormsApp2/Form1.cs
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -16,5 +16,78 @@ namespace WindowsFormsApp2
         {
             InitializeComponent();
         }
+
+        private void btnTroca_Click(object sender, EventArgs e)
+        {
+
+            string valor1 = txbValor1.Text;
+            string valor2 = txbValor2.Text;
+            string valor3 = txbValor3.Text;
+
+            txbValor1.Text = valor3;
+            txbValor2.Text = valor1;
+            txbValor3.Text = valor2;
+
+        }
+
+        private void btnDecrescente_Click(object sender, EventArgs e)
+        {
+
+            float valor1 = float.Parse(txbValor1.Text);
+            float valor2 = float.Parse(txbValor2.Text);
+            float valor3 = float.Parse(txbValor3.Text);
+
+            //verificando o maior numero
+            if ( valor1 > valor2 && valor1 > valor3 )
+            {
+                txbValor1.Text = valor1.ToString();
+            }
+            
+            else if ( valor2 > valor1 && valor2 > valor3 )
+            {
+                txbValor1.Text = valor2.ToString();
+            }
+
+            else if( valor3 > valor1 && valor3 > valor2 )
+            {
+                txbValor1.Text = valor3.ToString();
+            }
+
+            //verificando o menor numero
+            if ( valor1 < valor2 && valor1 < valor3 )
+            {
+                txbValor3.Text = valor1.ToString();
+            }
+
+            else if ( valor2 < valor1 && valor2 < valor3 )
+            {
+                txbValor3.Text = valor2.ToString();
+            }
+
+            else if ( valor3 < valor1 && valor3 < valor2 )
+            {
+                txbValor3.Text = valor3.ToString();
+            }
+
+            //Achando o meio
+            if ( ( valor1 < valor2 && valor1 > valor3 ) || ( valor1 > valor2 && valor1 < valor3 ) )
+            {
+                txbValor2.Text = valor1.ToString();
+            }
+
+            else if ( ( valor2 < valor1 && valor2 > valor3 ) || ( valor2 > valor1 && valor2 < valor3 ) )
+            {
+                txbValor2.Text = valor2.ToString();
+            }
+
+            else if ( ( valor3 < valor1 && valor3 > valor2 ) || ( valor3 > valor1 && valor3 < valor2 ) )
+            {
+                txbValor2.Text = valor3.ToString();
+            }
+
+
+
+
+        }
     }
 }
